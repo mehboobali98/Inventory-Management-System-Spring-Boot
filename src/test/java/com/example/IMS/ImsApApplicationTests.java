f57178c9de2120a1c0c71fe3380e26fab93a5291
mehboobali98 <57248707+mehboobali98@users.noreply.github.com>
2021-06-14 15:14:25 +0500

    Upload project files

---
+++ src/test/java/com/example/IMS/ImsApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
