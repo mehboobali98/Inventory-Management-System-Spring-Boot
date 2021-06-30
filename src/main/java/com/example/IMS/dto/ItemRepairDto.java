a0f6037a121e81012d657e1fd9fa5df31196d4bc
mehboobali98 <57248707+mehboobali98@users.noreply.github.com>
2021-06-30 16:49:36 +0500

    Update ItemRepairDto: Added new id field and its getters, setters

--- src/main/java/com/example/IMS/dto/ItemRepairDto.java
+++ src/main/java/com/example/IMS/dto/ItemRepairDto.java
@@ -1,21 +1,32 @@
 package com.example.IMS.dto;
 
 public class ItemRepairDto {
+	private long id;
 	private long itemId;
 	private long vendorId;
 	private int repairCost;
 	private double price;
-
-	public ItemRepairDto() {
-
+	
+	public ItemRepairDto()
+	{
+		
 	}
 
-	public ItemRepairDto(long itemId, long vendorId, int repairCost, double price) {
+	public ItemRepairDto(long id, long itemId, long vendorId, int repairCost, double price) {
 		super();
+		this.id = id;
 		this.itemId = itemId;
 		this.vendorId = vendorId;
 		this.repairCost = repairCost;
 		this.price = price;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public long getItemId() {
