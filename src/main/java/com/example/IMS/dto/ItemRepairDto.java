3b1f7694f1994009676cbf985c2d2a6bc999d918
mehboobali98 <57248707+mehboobali98@users.noreply.github.com>
2021-07-01 02:04:40 +0500

    added server side validation in create and edit views

--- src/main/java/com/example/IMS/dto/ItemRepairDto.java
+++ src/main/java/com/example/IMS/dto/ItemRepairDto.java
@@ -1,10 +1,31 @@
 package com.example.IMS.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class ItemRepairDto {
+
 	private long id;
+
+	@NotNull(message = "Item ID is mandatory")
+	@Min(value = 1, message = "Item ID must be greater than 0")
+	@Max(value = Long.MAX_VALUE, message = "Item ID must be lesser than " + Long.MAX_VALUE)
 	private long itemId;
+
+	@NotNull(message = "Vendor ID is mandatory")
+	@Min(value = 1, message = "Vendor ID must be greater than 0")
+	@Max(value = Long.MAX_VALUE, message = "Vendor ID must be lesser than " + Long.MAX_VALUE)
 	private long vendorId;
+
+	@NotNull(message = "Repair Cost is mandatory")
+	@Min(value = 1, message = "Repair Cost must be greater than 0")
+	@Max(value = (long) Double.MAX_VALUE, message = "Repair Cost must be lesser than " + Double.MAX_VALUE)
 	private double repairCost;
+
+	@NotNull(message = "Item Price is mandatory")
+	@Min(value = 1, message = "Item Price must be greater than 0")
+	@Max(value = (long) Double.MAX_VALUE, message = "Item Price must be lesser than " + Double.MAX_VALUE)
 	private double price;
 
 	public ItemRepairDto() {
