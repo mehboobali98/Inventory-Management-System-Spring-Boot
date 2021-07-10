a301f5317396d98024d43a1345a3e7f10c82d564
mehboobali98 <57248707+mehboobali98@users.noreply.github.com>
2021-07-10 20:15:34 +0500

    Update Helper.java - added method to compute dueDate

--- src/main/java/com/example/IMS/Utilities/Helper.java
+++ src/main/java/com/example/IMS/Utilities/Helper.java
@@ -1,14 +1,37 @@
 package com.example.IMS.Utilities;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Helper {
 
+	private static DateFormat dateFormat;
+
+	static {
+		dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	}
+
 	public static String getCurrentTime() {
-		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Calendar cal = Calendar.getInstance();
 		return dateFormat.format(cal.getTime());
+	}
+
+	public static String getDueDate(String issueDate, int loanPeriod) {
+		Calendar cal = Calendar.getInstance();
+		try {
+			cal.setTime(dateFormat.parse(issueDate));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		cal.add(Calendar.DATE, loanPeriod);
+		return convertDateToString(cal.getTime());
+	}
+
+	private static String convertDateToString(Date date) {
+		return dateFormat.format(date);
 	}
 }
