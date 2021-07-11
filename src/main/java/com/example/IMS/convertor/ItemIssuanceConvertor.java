5b6b69d27899ffb72a84ee65c21573308cd58e58
mehboobali98 <57248707+mehboobali98@users.noreply.github.com>
2021-07-11 16:41:35 +0500

    create itemIssuanceConvertor class for dto to model conversion and vice versa

---
+++ src/main/java/com/example/IMS/convertor/ItemIssuanceConvertor.java
@@ -0,0 +1,51 @@
+package com.example.IMS.convertor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.IMS.dto.ItemIssuanceDto;
+import com.example.IMS.Utilities.Helper;
+import com.example.IMS.model.Loan;
+import com.example.IMS.service.BorrowerService;
+import com.example.IMS.service.ItemService;
+
+@Component
+public class ItemIssuanceConvertor {
+
+	@Autowired
+	private ItemService itemService;
+	
+	@Autowired
+	private BorrowerService borrowerService;
+
+	public ItemIssuanceDto modelToDto(Loan loan) {
+		ItemIssuanceDto dto = new ItemIssuanceDto();
+		dto.setId(loan.getId());
+		dto.setLoanDuration(loan.getLoanDuration());
+		dto.setIssueDate(loan.getIssueDate());
+		dto.setDueDate(Helper.getDueDate(loan.getIssueDate(), loan.getLoanDuration()));
+		dto.setFineAmount(loan.getFineAmount());
+		dto.setItemId(itemService.findItemIdByLoanId(loan.getId()));
+		dto.setBorrowerId(borrowerService.getBorrowerIdByLoanId(loan.getId()));
+		return dto;
+	}
+
+	public Loan dtoToModel(ItemIssuanceDto dto) {
+		Loan loan = new Loan();
+		loan.setLoanDuration(dto.getLoanDuration());
+		loan.setIssueDate(Helper.getCurrentTime());
+		return loan;
+	}
+
+	public List<ItemIssuanceDto> modelToDto(List<Loan> loanList) {
+		List<ItemIssuanceDto> DtoList = new ArrayList<>();
+		int size = loanList.size();
+		for (int i = 0; i < size; i++) {
+			DtoList.add(modelToDto(loanList.get(i)));
+		}
+		return DtoList;
+	}
+}
