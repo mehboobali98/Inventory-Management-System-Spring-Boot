ac9563fed8c9c08a6144e59fdfc721566f45c4d6
mehboobali98 <57248707+mehboobali98@users.noreply.github.com>
2021-07-11 17:43:35 +0500

    update itemIssuanceDto - added annotations for server side validation

--- src/main/java/com/example/IMS/dto/ItemIssuanceDto.java
+++ src/main/java/com/example/IMS/dto/ItemIssuanceDto.java
@@ -1,10 +1,30 @@
 package com.example.IMS.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class ItemIssuanceDto {
 	private long id;
+
+	@NotNull(message = "Item ID is mandatory")
+	@Min(value = 1, message = "Item ID must be greater than 0")
+	@Max(value = Long.MAX_VALUE, message = "Item ID must be lesser than " + Long.MAX_VALUE)
 	private long itemId;
+
+	@NotNull(message = "Vendor ID is mandatory")
+	@Min(value = 1, message = "Vendor ID must be greater than 0")
+	@Max(value = Long.MAX_VALUE, message = "Vendor ID must be lesser than " + Long.MAX_VALUE)
 	private long borrowerId;
+
+	@NotNull(message = "Loan Duration is mandatory")
+	@Min(value = 1, message = "Loan Duration must be greater than 0")
+	@Max(value = Long.MAX_VALUE, message = "Loan Duration must be lesser than " + 21)
 	private long loanDuration;
+
+	@NotNull(message = "Fine Amount is mandatory")
+	@Min(value = 1, message = "Fine Amount must be greater than 0")
+	@Max(value = (long) Double.MAX_VALUE, message = "Fine Amount must be lesser than " + Double.MAX_VALUE)
 	private double fineAmount;
 	private String issueDate;
 	private String dueDate;
--- src/main/java/com/example/IMS/model/Borrower.java
+++ src/main/java/com/example/IMS/model/Borrower.java
@@ -3,7 +3,6 @@ package com.example.IMS.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
