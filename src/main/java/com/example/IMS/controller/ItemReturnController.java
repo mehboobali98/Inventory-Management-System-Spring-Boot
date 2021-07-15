957c7fb2074665f02ae97e36eef8c14395a4b90f
mehboobali98 <57248707+mehboobali98@users.noreply.github.com>
2021-07-15 18:55:56 +0500

    update item return controller - added validation in create method

--- src/main/java/com/example/IMS/controller/ItemReturnController.java
+++ src/main/java/com/example/IMS/controller/ItemReturnController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,10 +53,27 @@ public class ItemReturnController {
 	@PostMapping("/ItemReturnCreate")
 	public String Create(@Valid @ModelAttribute("itemReturnDto") ItemReturnDto itemReturnDto, BindingResult result) {
 		itemReturnDto.setReturnDate(Helper.getCurrentTime());
-		Borrower borrower = borrowerService.getBorrowerById(itemReturnDto.getBorrowerId());
-		Item item = itemService.getItemById(itemReturnDto.getItemId());
-		Loan loan = itemIssuanceService.findItemIssued(borrower.getId(), item.getId());
+		Borrower borrower = null;
+		Item item = null;
+		Loan loan = null;
+
+		String err = borrowerService.validateBorrowerId(itemReturnDto.getBorrowerId());
+		if (!err.isEmpty()) {
+			ObjectError error = new ObjectError("globalError", err);
+			result.addError(error);
+		} else {
+			borrower = borrowerService.getBorrowerById(itemReturnDto.getBorrowerId());
+		}
+		err = itemService.validateItemId(itemReturnDto.getItemId());
+		if (!err.isEmpty()) {
+			ObjectError error = new ObjectError("globalError", err);
+			result.addError(error);
+		} else {
+			item = itemService.getItemById(itemReturnDto.getItemId());
+		}
+
 		try {
+			loan = itemIssuanceService.findItemIssued(borrower.getId(), item.getId());
 			borrower.removeLoan(loan);
 			item.removeLoan(loan);
 			loan.setReturnDate();
@@ -63,6 +81,12 @@ public class ItemReturnController {
 			itemIssuanceService.saveItemIssued(loan);
 		} catch (NullPointerException e) {
 			System.out.println("Null Pointer Exception Caught in Item Return Controller.");
+			err = "Loan ID does not exist. Invalid input";
+			ObjectError error = new ObjectError("globalError", err);
+			result.addError(error);
+		}
+		if (result.hasErrors()) {
+			return "/Item Return/Create";
 		}
 		return "redirect:/ItemReturnView";
 	}
