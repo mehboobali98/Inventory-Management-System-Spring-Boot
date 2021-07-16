403259b941524b55941f80db6559a398305bf341
mehboobali98 <57248707+mehboobali98@users.noreply.github.com>
2021-07-16 17:58:08 +0500

    implement client and server side validations

--- src/main/java/com/example/IMS/controller/FineController.java
+++ src/main/java/com/example/IMS/controller/FineController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,9 +50,26 @@ public class FineController {
 	@PostMapping("/FinePayment/{borrowerId}")
 	public String FinePayment(@PathVariable(value = "borrowerId") long borrowerId,
 			@Valid @ModelAttribute("fineDto") FineDto fineDto, BindingResult result) {
-		Borrower borrower = borrowerService.getBorrowerById(fineDto.getBorrowerId());
-		borrower.updateFine(fineDto.getFinePaid());
-		borrowerService.updateBorrower(borrower);
+		Borrower borrower = null;
+		String err = borrowerService.validateBorrowerId(fineDto.getBorrowerId());
+		if (!err.isEmpty()) {
+			ObjectError error = new ObjectError("globalError", err);
+			result.addError(error);
+		} else {
+			borrower = borrowerService.getBorrowerById(fineDto.getBorrowerId());
+		}
+		if (result.hasErrors()) {
+			return "/Fine/Create";
+		}
+		try {
+			borrower.updateFine(fineDto.getFinePaid());
+			borrowerService.updateBorrower(borrower);
+		} catch (Exception e) {
+			System.out.println("Exception caught in Fine Controller.");
+			err = "Unable to update borrower fine details.";
+			ObjectError error = new ObjectError("globalError", err);
+			result.addError(error);
+		}
 		return "redirect:/FineView";
 	}
 }
--- src/main/java/com/example/IMS/dto/FineDto.java
+++ src/main/java/com/example/IMS/dto/FineDto.java
@@ -1,8 +1,20 @@
 package com.example.IMS.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class FineDto {
+	@NotNull(message = "Borrower ID is mandatory")
+	@Min(value = 1, message = "Borrower ID must be greater than 0")
+	@Max(value = Long.MAX_VALUE, message = "Borrower ID must be lesser than " + Long.MAX_VALUE)
 	private long borrowerId;
+
 	private double totalFine;
+
+	@NotNull(message = "Fine Amount Paid is mandatory")
+	@Min(value = 0, message = "Fine Amount Paid must be greater than 0")
+	@Max(value = (long) Double.MAX_VALUE, message = "Fine Amount Paid must be lesser than " + Double.MAX_VALUE)
 	private double finePaid;
 
 	public FineDto() {
