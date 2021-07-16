37a2923698525217f12e08c5727a11c1cd207513
mehboobali98 <57248707+mehboobali98@users.noreply.github.com>
2021-07-16 17:17:28 +0500

    Create FineController - added method to create, view details and pay fine functionalities

---
+++ src/main/java/com/example/IMS/controller/FineController.java
@@ -0,0 +1,57 @@
+package com.example.IMS.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.IMS.dto.FineDto;
+import com.example.IMS.model.Borrower;
+import com.example.IMS.service.BorrowerService;
+import com.example.IMS.service.ItemIssuanceService;
+
+@Controller
+public class FineController {
+
+	@Autowired
+	private BorrowerService borrowerService;
+
+	@Autowired
+	private ItemIssuanceService itemIssuanceService;
+
+	@GetMapping("/FineView")
+	public String Index(Model model) {
+		model.addAttribute("itemsWithFineList", itemIssuanceService.getItemsWithFine());
+		return "/Fine/View";
+
+	}
+
+	@GetMapping("/FineCreate")
+	public String Create(Model model) {
+		FineDto fineDto = new FineDto();
+		model.addAttribute("fineDto", fineDto);
+		return "/Fine/Create";
+	}
+
+	@PostMapping("/FineDetails")
+	public String Details(@Valid @ModelAttribute("fineDto") FineDto fineDto, BindingResult result) {
+		Borrower borrower = borrowerService.getBorrowerById(fineDto.getBorrowerId());
+		double totalFine = borrower.totalFine();
+		fineDto.setTotalFine(totalFine);
+		return "/Fine/Details";
+	}
+
+	@PostMapping("/FinePayment/{borrowerId}")
+	public String FinePayment(@PathVariable(value = "borrowerId") long borrowerId,
+			@Valid @ModelAttribute("fineDto") FineDto fineDto, BindingResult result) {
+		Borrower borrower = borrowerService.getBorrowerById(fineDto.getBorrowerId());
+		borrower.updateFine(fineDto.getFinePaid());
+		return "redirect:/FineView";
+	}
+}
